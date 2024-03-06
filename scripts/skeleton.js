f609595888bb4be4ea887abe33300497f2152bb5
AlfredoLuzardo <luzardoneto2005@gmail.com>
2024-03-06 11:53:49 -0800

    Added sticky footer and footer to skeleton.js by Alfredo Luzardo

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -9,11 +9,12 @@ function loadSkeleton() {
             // User is signed in.
             // Do something for the user here.
             console.log($('#navbarPlaceholder').load('./text/nav_after_login.html'));
-            console.log($('#footerPlaceholder').load('./text/footer.html'));
+            console.log($('#stickyFooterPlaceholder').load('./text/sticky_footer.html'));
+            console.log($('#footerPlaceholder').load('./text/footer.html'))
         } else {
             // No user is signed in.
             console.log($('#navbarPlaceholder').load('./text/nav_before_login.html'));
-            console.log($('#footerPlaceholder').load('./text/footer.html'));
+            console.log($('#stickyFooterPlaceholder').load('./text/sticky_footer.html'));
         }
     });
 }
