50339f991010189ae73b0e6db661a661148e09fb
AlfredoLuzardo <luzardoneto2005@gmail.com>
2024-04-03 15:48:18 -0700

    Added fonts to the navbar, edited the map.js script to have a placeholder and fixed naming issues. Applied style to the popup, imported a pin image.

--- scripts/map.js
+++ scripts/map.js
@@ -26,7 +26,7 @@ function showMap() {
         // Add interactive pins for the hikes
         //---------------------------------
         addPostPins(map);
-        
+
     });
 }
 
@@ -34,88 +34,95 @@ showMap();
 
 function addPostPins(map) {
 
-    // READING information from "posts" collection in Firestore
-    db.collection('posts').get().then(allPosts => {
-        const features = []; // Defines an empty array for information to be added to
+    map.loadImage('./images/placeholder.png',
+        (error, image) => {
+            if (error) throw error;
 
-        allPosts.forEach(doc => {
-            // Extract coordinates of the place
-            coordinates = [doc.data().long, doc.data().lat];
-            console.log(coordinates);
-            // Extract other addition fields that you want etc.
-            event_name = doc.data().title; // Event Name
-            preview = doc.data().summary; // Text Preview
-            img = doc.data().image; // Image
-            // url = doc.data().link; // URL
+            map.addImage('eventpin', image);
 
-            // Push information (properties, geometry) into the features array
-            features.push({
-                'type': 'Feature',
-                'properties': {
-                    'description': `<strong>${event_name}</strong><p>${preview}</p> 
-                            <br> <img src=${img}> <br> <a href="/clicked_post.html?id=${doc.id}" target="_blank" 
-                            title="Opens in a new window">Read more</a>`
-                },
-                'geometry': {
-                    'type': 'Point',
-                    'coordinates': coordinates
-                }
+            // READING information from "posts" collection in Firestore
+            db.collection('posts').get().then(allPosts => {
+                const features = []; // Defines an empty array for information to be added to
+
+                allPosts.forEach(doc => {
+                    // Extract coordinates of the place
+                    coordinates = [doc.data().long, doc.data().lat];
+                    console.log(coordinates);
+                    // Extract other addition fields that you want etc.
+                    title = doc.data().title; // Event Name
+                    summary = doc.data().summary; // Text Preview
+                    img = doc.data().image; // Image
+                    // url = doc.data().link; // URL
+
+                    // Push information (properties, geometry) into the features array
+                    features.push({
+                        'type': 'Feature',
+                        'properties': {
+                            'description': `<strong>${title}</strong><p>${summary}</p> 
+                            <div><img src="https://fakeimg.pl/130x100/cccccc/636363?text=PLACE+HOLDER"></div>
+                            <br> <div id="anchor-div"><a href="/clicked_post.html?id=${doc.id}" target="_blank" 
+                            title="Opens in a new window">READ MORE</a></div>`
+                        },
+                        'geometry': {
+                            'type': 'Point',
+                            'coordinates': coordinates
+                        }
+                    });
+                });
+
+                // Adds features (in our case, pins) to the map
+                // "places" is the name of this array of features
+                map.addSource('places', {
+                    'type': 'geojson',
+                    'data': {
+                        'type': 'FeatureCollection',
+                        'features': features
+                    }
+                });
+
+                // Creates a layer above the map displaying the pins
+                // Add a layer showing the places.
+                map.addLayer({
+                    'id': 'places',
+                    'type': 'symbol', // what the pins/markers/points look like
+                    'source': 'places',
+                    'layout': {   // customize colour and size
+                        'icon-image': 'eventpin',
+                        'icon-size': 0.05,
+                        'icon-allow-overlap': true
+                    }
+                });
+
+                // When one of the "places" markers are clicked,
+                // create a popup that shows information 
+                // Everything related to a marker is save in features[] array
+                map.on('click', 'places', (e) => {
+                    // Copy coordinates array.
+                    const coordinates = e.features[0].geometry.coordinates.slice();
+                    const description = e.features[0].properties.description;
+
+                    // Ensure that if the map is zoomed out such that multiple 
+                    // copies of the feature are visible, the popup appears over 
+                    // the copy being pointed to.
+                    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+                    }
+
+                    new mapboxgl.Popup()
+                        .setLngLat(coordinates)
+                        .setHTML(description)
+                        .addTo(map);
+                });
+
+                // Change the cursor to a pointer when the mouse hovers over the places layer.
+                map.on('mouseenter', 'places', () => {
+                    map.getCanvas().style.cursor = 'pointer';
+                });
+
+                // Defaults cursor when not hovering over the places layer
+                map.on('mouseleave', 'places', () => {
+                    map.getCanvas().style.cursor = '';
+                });
             });
-        });
-
-        // Adds features (in our case, pins) to the map
-        // "places" is the name of this array of features
-        map.addSource('places', {
-            'type': 'geojson',
-            'data': {
-                'type': 'FeatureCollection',
-                'features': features
-            }
-        });
-
-        // Creates a layer above the map displaying the pins
-        // Add a layer showing the places.
-        map.addLayer({
-            'id': 'places',
-            'type': 'circle', // what the pins/markers/points look like
-            'source': 'places',
-            'paint': {   // customize colour and size
-                'circle-color': '#4264fb',
-                'circle-radius': 6,
-                'circle-stroke-width': 2,
-                'circle-stroke-color': '#ffffff'
-            }
-        });
-
-        // When one of the "places" markers are clicked,
-        // create a popup that shows information 
-        // Everything related to a marker is save in features[] array
-        map.on('click', 'places', (e) => {
-            // Copy coordinates array.
-            const coordinates = e.features[0].geometry.coordinates.slice();
-            const description = e.features[0].properties.description;
-
-            // Ensure that if the map is zoomed out such that multiple 
-            // copies of the feature are visible, the popup appears over 
-            // the copy being pointed to.
-            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-                coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-            }
-
-            new mapboxgl.Popup()
-                .setLngLat(coordinates)
-                .setHTML(description)
-                .addTo(map);
-        });
-
-        // Change the cursor to a pointer when the mouse hovers over the places layer.
-        map.on('mouseenter', 'places', () => {
-            map.getCanvas().style.cursor = 'pointer';
-        });
-
-        // Defaults cursor when not hovering over the places layer
-        map.on('mouseleave', 'places', () => {
-            map.getCanvas().style.cursor = '';
-        });
-    });
+        })
 }
